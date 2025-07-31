459ffe9a0c717fb194d118a777ffff0c076122c5
Julian <JulianDroske@users.noreply.github.com>
2025-07-31 23:47:47 +0800

    make: better hehaviour while supporting lazy compilation
    The resulting ELF file has been moved to build/ (by default).
    
    For supported envars, see the CONFIG section in make.sh.

--- make.sh
+++ make.sh
@@ -1,22 +1,63 @@
 #!/bin/sh
 
-TMPDIR="$(mktemp -d -t osbpb-make.XXXXXX)"
+
+# ----------------------------------------------------------------------------
+#
+# CONFIG
+#
+# ----------------------------------------------------------------------------
+
+OUTDIR="${OUTDIR:-build}"
+OUT="${OUT:-${OUTDIR}/osbpb}"
 
 CC="${CC:-cc}"
+# pure linking with ld without additional args would lose the `_start` symbol,
+# so use CC for now
+LD="${LD:-${CC}}"
+
 LUAVER="${LUAVER:-5.4}"
 LUA="${LUA:-lua${LUAVER}}"
 
-CFLAGS="${CFLAGS:- -std=c99}"
-LDFLAGS="${LDFLAGS} -lm"
 IFLAGS="${IFLAGS}"
+CFLAGS="${CFLAGS:- -std=c99}"
+LDFLAGS="${LDFLAGS} -lm -lc"
+
+
+# ----------------------------------------------------------------------------
+#
+# UTIL / TRAP / SAFETY BELT
+#
+# ----------------------------------------------------------------------------
+
+print_raw() {
+  printf '%s' "${*}"
+}
+
+log_error() {
+  echo "[ERROR] ${*}" >&2
+}
+
+log_info() {
+  echo "[INFO] ${*}" >&2
+}
+
+log_compile() {
+  tag="${1}"
+  shift 1
+  echo "[${tag}] ${*}" >&2
+}
+
+try_mkdir() {
+  test -d "${1}" || mkdir -p "${1}"
+}
+
 
 cleanup(){
   trap - EXIT
-  rm -rf "${TMPDIR}"
 }
 
 panic(){
-  test -n "${*}" && echo "${*}" >&2
+  test -n "${*}" && echo "[FATAL] ${*}" >&2
   echo '[BUILD FAILURE]'
   cleanup
   exit 1
@@ -24,6 +65,13 @@ panic(){
 
 set -e
 trap 'panic' EXIT
+
+
+# ----------------------------------------------------------------------------
+#
+# MAIN PROGRAM
+#
+# ----------------------------------------------------------------------------
 
 cd "$(dirname $0)"
 
@@ -34,6 +82,8 @@ SRCDIR="${PROJDIR}/src"
 SRCLUADIR="${SRCDIR}/lua"
 
 SRCS="$(find ${SRCDIR} -maxdepth 1 -name '*.c')"
+# filled by compile_*
+OBJS=
 
 LIBDIR="${PROJDIR}/lib"
 
@@ -42,22 +92,73 @@ SRCS="${SRCS} ${LIB_LUA_DIR}/onelua.c"
 CFLAGS="${CFLAGS} -DMAKE_LIB=1"
 IFLAGS="${IFLAGS} -I${LIB_LUA_DIR}"
 
-# LIB_ARGPARSE_DIR="${LIBDIR}/argparse"
+# weird behaviour: reading and writting has different result under test,
+# cat and output redirection, so added {} to avoid it
+CONFIG="{
+  CC ${CC}
+  LD ${LD}
+  LUAVER ${LUAVER}
+  LUA ${LUA}
+  IFLAGS ${IFLAGS}
+  CFLAGS ${CFLAGS}
+  LDFLAGS ${LDFLAGS}
+}"
+CONFIG_CACHE_FILE="${OUTDIR}/.config"
 
-# LIB_LUAPOSIX_DIR="${LIBDIR}/luaposix"
-# SRCS="${SRCS} $(find ${LIB_LUAPOSIX_DIR}/ext -name '*.c'))"
-# IFLAGS="${IFLAGS} -I${LIB_LUAPOSIX_DIR}/ext/include"
 
-  # -C "${LIB_LUAPOSIX_DIR}/lib" posix -i posix.version "return 'posix for lua, bundled in osbpb'" \
-"${LUA}" "${TOOLDIR}/onec.lua" -o "${TMPDIR}/osbpb.lua" -m osbpb.init \
+# -- MAIN PROCEDURE ----------------------------------------------------------
+
+try_mkdir "${OUTDIR}" ||
+  panic "unable to create output directory '${OUTDIR}'"
+
+IS_CONFIG_SAME=n
+test -f "${CONFIG_CACHE_FILE}" &&
+  test "$(cat ${CONFIG_CACHE_FILE})" = "${CONFIG}" &&
+  IS_CONFIG_SAME=y
+
+HAS_SOURCE_CHANGE=n
+
+compile_c_obj() {
+  source="${1}"
+  source_name_nosuffix="$(basename ${source%%.c})"
+  objfile="${OUTDIR}/${source_name_nosuffix}.o"
+  OBJS="${OBJS} ${objfile}"
+  timestamp="$(stat -c %Y ${source})"
+  timestamp_cache_file="${OUTDIR}/.timestamp.${source_name_nosuffix}"
+
+  test "${IS_CONFIG_SAME}" = y &&
+    test -f "${timestamp_cache_file}" &&
+    test "$(cat ${timestamp_cache_file})" = "${timestamp}" &&
+    return 0
+
+  log_compile CC "${objfile}"
+  "${CC}" ${IFLAGS} ${CFLAGS} -c -o "${objfile}" "${source}"
+  HAS_SOURCE_CHANGE=y
+  # update timestamp cache
+  print_raw "${timestamp}" >"${timestamp_cache_file}"
+}
+
+
+# generate header for osbpb.lua binary for embedding
+"${LUA}" "${TOOLDIR}/onec.lua" -o "${OUTDIR}/osbpb.lua" -m osbpb.init \
   -C "${SRCLUADIR}" .
-mkdir "${TMPDIR}/include"
-"${LUA}" "${TOOLDIR}/bin2c.lua" -o "${TMPDIR}/include/osbpb-lua.h" -n osbpb_lua "${TMPDIR}/osbpb.lua"
-IFLAGS="${IFLAGS} -I${TMPDIR}/include"
+try_mkdir "${OUTDIR}/include"
+"${LUA}" "${TOOLDIR}/bin2c.lua" -o "${OUTDIR}/include/osbpb-lua.h" \
+  -n osbpb_lua "${OUTDIR}/osbpb.lua"
+IFLAGS="${IFLAGS} -I${OUTDIR}/include"
 
-"${CC}" "-I${TMPDIR}/include" ${IFLAGS} ${CFLAGS} -o "${TMPDIR}/osbpb" ${SRCS} ${LDFLAGS}
-cp "${TMPDIR}/osbpb" "${PROJDIR}/"
+for srcfile in ${SRCS}; do
+  compile_c_obj "${srcfile}"
+done
+
+# link
+if test "${HAS_SOURCE_CHANGE}" = y; then
+  log_compile LD "${OUT}"
+  "${LD}" -o "${OUT}" ${OBJS} ${LDFLAGS}
+fi
 
 cleanup
 echo '[BUILD SUCCESS]'
+
+print_raw "${CONFIG}" >"${CONFIG_CACHE_FILE}"
 
